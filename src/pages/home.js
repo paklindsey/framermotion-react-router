12b6639f4cae3fa5ae5cc07e2c1ce4a2a61b3c3f
Lindsey Pak <paklindsey@gmail.com>
2021-06-09 14:18:40 -0600

    first three aniamtions

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,37 +1,53 @@
-import * as React from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import ProgressiveImage from "react-progressive-image";
+import { motion } from "framer-motion";
+
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
 const Home = ({ imageDetails, image }) => (
   <>
     <main>
-      <div className='container'>
-        <div className='row center'>
-          <div className='image-container'>
+      <div exit={{ opacity: 0 }} transition={transition} className="container">
+        <div className="row center">
+          <div className="image-container">
             <div
-              className='thumbnail'
+              className="thumbnail"
               ref={image}
               style={{
                 width: imageDetails.width,
                 height: imageDetails.height,
-              }}>
-              <div className='frame'>
+              }}
+            >
+              <div className="frame">
                 <Link to={`/model/yasmeen-tariq`}>
                   <ProgressiveImage
                     src={require("../images/yasmeen.webp")}
-                    placeholder={require("../images/compressed-image.jpg")}>
-                    {(src) => <img src={src} alt='Yasmeen Tariq' />}
+                    placeholder={require("../images/compressed-image.jpg")}
+                  >
+                    {(src) => (
+                      <motion.img
+                        whileHover={{ scale: 1.1 }}
+                        transition={transition}
+                        src={src}
+                        alt="Yasmeen Tariq"
+                      />
+                    )}
                   </ProgressiveImage>
                 </Link>
               </div>
             </div>
-            <div className='information'>
-              <div className='title'>Yasmeen Tariq</div>
-              <div className='location'>
+            <motion.div
+              exit={{ opacity: 0 }}
+              transition={transition}
+              className="information"
+            >
+              <div className="title">Yasmeen Tariq</div>
+              <div className="location">
                 <span>28.538336</span>
                 <span>-81.379234</span>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
